06f98aedfae162e66da5ff3350e91dfd5a6f58e6
BaiYuan <hoshino.1786@gmail.com>
2025-02-22 14:14:14 +0800

    V0.1

---
+++ README.md
@@ -0,0 +1,109 @@
+# ZZZ自动化钓鱼工具
+
+## 1 项目简介
+
+使用paddleocr实现的zzz自动化钓鱼工具, 能够在钓鱼界面实现自动化的状态识别, 并持续钓鱼, 直到用户停止
+
+
+
+## 2 运行环境
+
+1. 项目使用python3.12.3开发, 其他版本python可能也可, 可以尝试
+
+2. 项目依赖部分三方库, 如果是conda环境, 使用下面命令安装:
+
+   ```
+   conda install keyboard
+   conda install numpy
+   conda install opencv
+   conda install pillow
+   conda install pyautogui
+   conda install pywin32
+   ```
+
+   paddleocr经过验证无法用conda安装, 改用pip安装也能用
+
+   ```
+   pip install paddlepaddle
+   pip install paddleocr==2.7.3
+   ```
+
+    如果是pip环境, 把上面的conda install改成pip install即可
+
+   ```
+   pip install keyboard
+   pip install numpy
+   pip install opencv
+   pip install pillow
+   pip install pyautogui
+   pip install pywin32
+   ```
+
+   关于conda和pip的使用方法请百度搜索
+
+   
+
+## 3 运行代码
+
+​	配好环境后, 按下面步骤运行代码即可
+
+1. 打开"绝区零.exe", 推荐将分辨率调整到1280*720窗口, 其他分辨率应该也可以运行
+
+2. 将场景移动到钓鱼界面, 石礁钓点和近海钓点均可
+
+3. 直接运行本项目的main.py文件
+
+   ```
+   python main.py
+   ```
+
+   出现类似于下面的输出代表代码正常运行:
+
+   ```
+   [2025/02/22 14:03:38] ppocr WARNING: Since the angle classifier is not initialized, it will not be used during the forward process
+   ocrList= ['绝区零', 'x', 'level12', 'uid:14344493 -1']
+   UNKNOWEN_STATE= None, continue
+   
+   
+   [2025/02/22 14:03:39] ppocr WARNING: Since the angle classifier is not initialized, it will not be used during the forward process
+   ocrList= ['绝区零', 'x', 'level12', 'uid:14344493 -1']
+   STATE= "None", 状态无变化
+   
+   [2025/02/22 14:03:39] ppocr WARNING: Since the angle classifier is not initialized, it will not be used during the forward process
+   ocrList= ['绝区零', 'x', 'level12', 'uid:14344493 -1']
+   STATE= "None", 状态无变化
+   ```
+
+4. 再点击一下绝区零窗口, 确保是zzz程序处在最前端并被操作, 代码运行期间不能移动窗口
+5. 如需停止, **按F10即可**
+
+## 4 注意事项
+
+1. 程序长时间运行可能出现窗口不响应脚本输入的情况, 这时候推荐重启zzz和脚本
+2. 脚本使用paddleocr作为ocr模型, 因为模型限制, 请将本脚本移动到 **全英文目录** 下执行, 否则报错
+3. 由于不同电脑的响应速度不同, 可能会出现上鱼操作失败的情况(等待紫色圈圈合拢点F的操作), 如果这种操作总是失败, 你可以尝试修改源码的**FISH_DELAY_TIME**
+
+```
+import cv2
+import keyboard
+import time
+import win32gui
+import random
+import pyautogui
+
+from PIL import ImageGrab
+from paddleocr import PaddleOCR
+from pathlib import Path
+
+import numpy as np
+
+rootDir = Path(__file__).parent.resolve()
+paddleOCRModlePath = rootDir / ".paddleocr"
+
+FISH_DELAY_TIME = 1.3 # 等待上鱼的wait时间, 修改这个值
+
+class FishBot:
+
+  """基于OCR实现的ZZZ钓鱼机器人
+```
+
---
+++ main.py
@@ -0,0 +1,280 @@
+##########################################################################################################
+#   Description: 钓鱼主程序
+#   Authors:     BaiYuan <395642104@qq.com>
+##########################################################################################################
+
+import cv2
+import keyboard
+import time
+import win32gui
+import random
+import pyautogui
+
+from PIL import ImageGrab
+from paddleocr import PaddleOCR
+from pathlib import Path
+
+import numpy as np
+
+rootDir = Path(__file__).parent.resolve()
+paddleOCRModlePath = rootDir / ".paddleocr"
+
+FISH_DELAY_TIME = 1.3 # 等待上鱼的wait时间
+
+class FishBot:
+    """基于OCR实现的ZZZ钓鱼机器人
+    """    
+    def __init__(self):
+        """初始化基础参数
+        """        
+        self.running = True
+        self.fishNum = 0
+        self.last_action_time = 0
+        self.struggle_start = 0
+        self.debug_counter = 0
+
+        self.lastState = None
+        self.newState = None
+        
+        # OCR初始化
+        try:
+            self.ocr = PaddleOCR(det_model_dir = str(paddleOCRModlePath / "whl" / "det"/ "ch"/ "ch_PP-OCRv4_det_infer"), 
+                        rec_model_dir = str(paddleOCRModlePath / "whl" / "rec" / "ch" / "ch_PP-OCRv4_rec_infer"), 
+                        cls_model_dir = str(paddleOCRModlePath / "whl" / "cls" / "ch_ppocr_mobile_v2.0_cls_infer"),
+                        lang = 'ch', show_log = False)
+
+        except Exception as e:
+            print(f"ocr模型初始化失败:{str(e)}")
+            raise e
+        
+        # 窗口控制参数
+        self.hwnd = win32gui.FindWindow(None, "绝区零")
+        if not self.hwnd:
+            raise Exception("游戏窗口未找到，请先启动游戏")
+        
+    def captureWinodow(self):
+        """捕获图像
+        """
+
+        # 获取窗口坐标
+        rect = win32gui.GetWindowRect(self.hwnd)
+        self.window = {
+            'left': rect[0],
+            'top': rect[1],
+            'width': rect[2] - rect[0],
+            'height': rect[3] - rect[1]
+        } 
+
+        try:
+            # 计算截取区域（left, top, right, bottom）
+            region = (
+                self.window['left'],
+                self.window['top'],
+                self.window['left'] + self.window['width'],
+                self.window['top'] + self.window['height']
+            )
+            
+            # 带重试机制的截图（最多尝试3次）
+            for attempt in range(3):
+                try:
+                    screenshot = ImageGrab.grab(bbox=region, all_screens=True)
+                    frame = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+                        
+                    return frame
+                except Exception as e:
+                    if attempt == 2:
+                        raise
+                    print(f"截图失败第{attempt+1}次尝试...")
+                    time.sleep(0.1)
+                    
+        except Exception as e:
+            print(f"最终截图失败：{str(e)}")
+            return None
+        
+    def ocrHandle(self, frame: cv2.typing.MatLike):
+        """获得的frame进行ocr处理
+
+        :param frame: 截图帧
+        """        
+        if frame is None:
+            return []
+        
+        try:
+            result = self.ocr.ocr(frame, cls=True)[0]
+
+            # print(result)
+            if not result:
+                return []
+            
+            # 拼接获得的str组成的列表
+            ocrList =  [line[1][0].lower().strip() for line in result if line[1] and line[1][0]]
+            if "长按" in ocrList and "a" in ocrList:
+                ocrList.append("长按a")
+            elif "长按" in ocrList and "d" in ocrList:
+                ocrList.append("长按d")
+            elif "连点" in ocrList and "a" in ocrList:
+                ocrList.append("连点a")
+            elif "连点" in ocrList and "d" in ocrList:
+                ocrList.append("连点d")
+
+            return ocrList
+
+        except Exception as e:
+            print(f"OCR异常：{str(e)}")
+            return []
+        
+    # =============== 基本操作函数 =============== #
+    def holdKey(self, key: str, duration: int):
+        """稳定长按
+
+        :param key: 按键名称
+        :param duration: 持续时间(s)
+        """
+        keyboard.press(key)
+        time.sleep(duration)
+        keyboard.release(key)
+        time.sleep(0.3)  # 操作后冷却
+    
+    def spamKey(self, key: str, times: int):
+        """随机间隔连击
+
+        :param key: 按键名称
+        :param times: 连续点按次数
+        """        
+        for _ in range(times):
+            keyboard.press(key)
+            time.sleep(0.02 + random.random()*0.03)
+            keyboard.release(key)
+            time.sleep(0.05 + random.random()*0.1)
+
+    def tabKey(self, key: str):
+        """点击按键一次
+
+        :param key: 按键名称
+        """        
+        keyboard.press(key)
+        time.sleep(0.05)
+        keyboard.release(key)
+
+    def stateManager(self, ocrList: list[str]):
+        """钓鱼状态机
+
+        :param ocrList: 使用OCR获取的字符串列表
+        """        
+
+        # 获取状态
+        stateMap = [
+            ("点击按键抛竿", "idle"),
+            ("等待上鱼", "wait"),
+            ("等待上", "wait"),
+            ("正确时机点击按键上鱼", "wait2"),
+            ("正确时机点击按键上", "wait2"),
+            ("正确时机", "wait2"),
+            ("点击空白处关闭", "restart"),
+            ("鱼跑了！", "fail"),
+            ("鱼跑了!", "fail"),
+            ("连点a", "连点a"),
+            ("连点d", "连点d"),
+            ("长按a", "长按a"),
+            ("长按d", "长按d"),
+        ]
+
+        for prompt, state in reversed(stateMap):
+            if prompt in ocrList:
+                self.newState = state
+                break
+        else:
+            self.newState = None
+
+        # 状态处理
+        if self.newState == self.lastState:
+            print(f"STATE= \"{self.newState}\", 状态无变化")
+            return
+        
+        else:
+            self.lastState = self.newState # 状态更新
+            if self.newState == "idle":
+                print(f"STATE= \"{self.newState}\", 准备抛竿")
+                self.tabKey(key = "f")
+
+            elif self.newState == "wait":
+                print(f"STATE= \"{self.newState}\", 正在等待上鱼")
+
+            elif self.newState == "wait2":
+                print(f"STATE= \"{self.newState}\", 准备在恰当时机点按F上鱼")
+                time.sleep(FISH_DELAY_TIME)
+                self.tabKey(key = "f")
+
+            elif self.newState == "连点a":
+                print(f"STATE= \"{self.newState}\", 进行连点A处理")
+                self.spamKey(key = "a", times = 10)
+
+            elif self.newState == "连点d":
+                print(f"STATE= \"{self.newState}\", 进行连点D处理")
+                self.spamKey(key = "d", times = 10)
+
+            elif self.newState == "长按a":
+                print(f"STATE= \"{self.newState}\", 进行长按A处理")
+                self.holdKey(key = "a", duration = 3)
+
+            elif self.newState == "长按d":
+                print(f"STATE= \"{self.newState}\", 进行长按D处理")
+                self.holdKey(key = "d", duration = 3)
+
+            elif self.newState == "restart":
+                self.fishNum += 1
+                print(f"STATE= \"{self.newState}\", 钓鱼成功, 当前已成功钓鱼 \"{self.fishNum}\" 次")
+
+                for _ in range(4):
+                    x = self.window['left'] + random.randint(100, self.window['width'] - 100)
+                    y = self.window['top'] + random.randint(100, self.window['height'] - 100)
+                    pyautogui.click(x, y)
+                    time.sleep(random.uniform(0.1, 0.3))
+
+            elif self.newState == "fail":
+                print(f"STATE= \"{self.newState}\", 钓鱼失败")
+
+                for _ in range(4):
+                    x = self.window['left'] + random.randint(100, self.window['width'] - 100)
+                    y = self.window['top'] + random.randint(100, self.window['height'] - 100)
+                    pyautogui.click(x, y)
+                    time.sleep(random.uniform(0.1, 0.3))
+
+            else:
+                print(f"UNKNOWEN_STATE= {self.newState}, continue")
+
+    def mainLoop(self):
+        """启动主循环
+        """      
+
+        keyboard.add_hotkey('f10', lambda: setattr(self, 'running', False))
+        print("自动钓鱼启动成功, F10停止")
+        
+        while self.running:
+            try:
+                
+                # 获取游戏画面
+                print("\n")
+                frame = self.captureWinodow()
+                if frame is None:
+                    time.sleep(1)
+                    continue
+
+                # 进行OCR处理
+                ocrList = self.ocrHandle(frame = frame)
+                print(f"ocrList= {ocrList}")
+
+                # 进行状态管理
+                self.stateManager(ocrList = ocrList)
+                
+                time.sleep(0.15)
+            except Exception as e:
+                print(f"运行时异常: {str(e)}")
+                self.running = False
+        
+        print("自动钓鱼已停止")
+
+
+if __name__ == "__main__":
+
+    FishBot().mainLoop()
